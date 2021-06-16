d596c8f84b05f7be3f13f66ce70ea5fab0fa88ed
TynysA <j.ady.tynys@gmail.com>
2021-06-16 11:13:28 +0600

    change in form

--- check.php
+++ check.php
@@ -1,7 +1,10 @@
 <?php
+//booking
 $name = $_POST["name"];
 $email = $_POST["email"];
 $phone = $_POST["phone"];
+$time=$_POST["time"];
+$count=$_POST["count"];
 //From contact block
 $c_name = $_POST["name_c"];
 $c_email = $_POST["email_c"];
@@ -11,7 +14,7 @@ $c_text = $_POST["text"];
 $massage="Name:" . "$c_name | $name \n". "Phone:"
     . "$c_phone | $phone \n" ."Email:" . "$c_email | $email \n"
     . "Text: " .$c_text;
-
+$massage .="\nTime: $time\n" ."Count of People: $count";
 $subject = "=?utf-8?B?" .base64_encode("Test massage") ."?=";
 $headers = " Form: $name\r\nContent-type: text/html; charset=utf-8\r\n";
 $to="j.ady.tynys@gmail.com";
